8e90bbd2f0997106d3e84cea5af950b479d156e6
Roy Hinojosa <roy_hinojosa@condenast.com>
2022-01-29 23:56:53 -0600

    feat(tests): adding tests

---
+++ src/components/ImageList.spec.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import endpointImagesAndDetails from '../static/endpointImagesAndDetails';
+import ImageList from './ImageList';
+
+test('on initial render, all image cards are shown on screen', () => {
+  render(<ImageList images={endpointImagesAndDetails}/>);
+
+  expect(screen.getByRole('img', {
+    name: /pic by Alejandro Escamilla/i,
+    name: /pic by Alejandro Escamilla/i,
+    name: /pic by paul jarvis/i,
+    name: /pic by Tina Rataj/i,
+    name: /pic by Lukas Budimaier/i
+  }))
+});
